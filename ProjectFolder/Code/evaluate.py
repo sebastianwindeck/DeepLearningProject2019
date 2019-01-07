bd4f6c188b8fe0902493edf1e11e4f2de7899477
Sebastian Windeck <sebastian.windeck@web.de>
2019-01-07 21:56:23 +0100

    Add parameters to evaluate functions

--- ProjectFolder/Code/evaluate.py
+++ ProjectFolder/Code/evaluate.py
@@ -1,10 +1,11 @@
 # Comment AS: die Funktionen sind sinnvoll; tendenziell sollten wir die aber nur für die "end-Auswertung" brauchen. Fürs
 # Bewerten der Noise-Kandidaten können wir evtl. direkt eine evaluate-Funktion des Modells (aus Keras) nutzen.
 # TODO: Malte check.
-def score():
-    # TODO: [Sebastian] Create function to use several scoring functions, f1, accuracy, precion
+def final_score(y_pred, y_true):
+    # TODO: [Sebastian] Create function to use several scoring functions, f1, accuracy, precision
+
     pass
 
-def pitch_confussion():
+def pitch_confussion(y_pred, y_true):
     # TODO: [Sebastian] Create a confusion matrix to get the confusion of pitches over a song
     pass
