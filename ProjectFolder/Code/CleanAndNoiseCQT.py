20488f87c9cd40cc65ed3ec9674b5235edb3845c
MusicProjHS2018 <45826072+MusicProjHS2018@users.noreply.github.com>
2019-01-19 10:49:17 +0100

    Add files via upload
    used to generate the plot CleanAndNoiseCQT.png : moonlight sonata clean, moonlight sonata + 0.02 white noise

---
+++ ProjectFolder/Code/CleanAndNoiseCQT.py
@@ -0,0 +1,48 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt 
+
+import acoustics
+from acoustics.generator import white
+
+import soundfile as sf
+
+import librosa
+from librosa import display
+
+fname = "../MAPS_MUS-mond_1_SptkBGAm.wav"
+data, fs = sf.read (fname)
+
+laenge = 500000 
+links = data[0:laenge,0]
+rechts = data[0:laenge,1]
+
+maxdata = np.max(links)
+mindata = np.min(links)
+
+ampl = 0.02 * ( maxdata - mindata )
+
+wh = ampl * white(laenge)
+
+y, sr = librosa.load(fname)
+#print (y.shape) 	# (8036879,)
+c = np.abs(librosa.cqt(y[0:laenge], sr=sr))
+#print (c.shape)   # (84, 977)  using y[0:laenge]
+
+plt.figure(figsize = (1,2))
+
+plt.subplot(121)
+librosa.display.specshow(librosa.amplitude_to_db(c, ref=np.max), sr=sr, y_axis = 'linear')
+plt.colorbar(format='%+2.0f dB')
+plt.title("CQT of clean wave")
+
+plt.subplot(122)
+d = np.abs(librosa.cqt(y[0:laenge] + wh[0:laenge], sr=sr))
+librosa.display.specshow(librosa.amplitude_to_db(d, ref=np.max), sr=sr, y_axis = 'linear')
+plt.colorbar(format='%+2.0f dB')
+plt.title("CQT of noised wave")
+plt.tight_layout()
+
+plt.show()
+
+
